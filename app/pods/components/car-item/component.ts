94579ef2957a8b18dfadb9b9c33abf11d5c44236
tlockhart <tony.lockhart@ymail.com>
2019-07-21 15:15:01 -0400

    fixed found message in car-item component.ts

--- app/pods/components/car-item/component.ts
+++ app/pods/components/car-item/component.ts
@@ -4,4 +4,9 @@ export default class CarItem extends Component.extend({
   // anything which *must* be merged to prototype here
 }) {
   // normal class body definition here
+  get shouldDisplay() {
+    // return this.args.isMakeFound;
+    return this.isMakeFound;
+  }
 };
+
